00f79d62109f0ebd13f3233665fe4c7d2dda37aa
endlessbounce <Ican.Iwill-2017github>
2018-06-27 22:52:49 +0300

    first commit

---
+++ src/main/java/by/endlessbounce/todo/AppBootStrap.java
@@ -0,0 +1,11 @@
+package by.endlessbounce.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBootStrap {
+    public static void main(String[] args) {
+        SpringApplication.run(AppBootStrap.class, args);
+    }
+}
