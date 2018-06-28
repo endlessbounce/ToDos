ae8c5b4a2612818137f9cddcf287732932c043ea
endlessbounce <Ican.Iwill-2017github>
2018-06-28 22:17:51 +0300

    added update functions and some modification restrictions

--- src/main/resources/static/js/todo.controller.js
+++ src/main/resources/static/js/todo.controller.js
@@ -12,11 +12,18 @@
             self.todos = [];
             self.errorMsg = "Please fill in all the fields";
             self.valid = false;
-            self.editMode = false;
 
-            //get form data on load of the page
+            //stores boolean values for switching edit mode buttons for each item
+            self.editMode = [];
+
+            //only one field can be edited at once
+            self.alreadyEditing = false;
+
+            //remember index of a currently edited item in case we delete it during editing
+            self.currentEdited = null;
+
             restService.findAllTodos().then(function (data) {
-                console.log("response data: " + data);
+                console.log(JSON.stringify(data));
                 self.todos = data;
             });
 
@@ -35,26 +42,66 @@
                 }
             }
 
-            self.deleteTodo = function (id) {
+            self.deleteTodo = function (id, index) {
                 restService.deleteToDo(id).then(function () {
                     restService.findAllTodos().then(function (data) {
                         self.todos = data;
+
+                        if (self.currentEdited === index) {
+                            self.toggleEditMode(index);
+                            self.currentEdited = null;
+                        }
+
+                        self.editMode.splice(index, 1);
                     });
                 });
             }
 
-            self.updateTodo = function (todo) {
+            self.setDone = function (todo) {
+               todo.isDone = true;
+               console.log(JSON.stringify(todo));
                 restService.updateToDo(todo).then(function () {
                     restService.findAllTodos().then(function (data) {
                         self.todos = data;
-                        self.changeEditMode();
                     });
                 });
             }
 
+            self.unDo = function (todo) {
+                todo.isDone = false;
+                restService.updateToDo(todo).then(function () {
+                    restService.findAllTodos().then(function (data) {
+                        self.todos = data;
+                    });
+                });
+            }
 
-            self.changeEditMode = function () {
-                self.editMode = self.editMode ? false : true;
+            self.updateTodo = function (todo, index) {
+                var editAllowed = self.alreadyEditing === true;
+                var nameOk = self.todos[index].name !== undefined;
+                var descriptionOk = self.todos[index].description !== undefined;
+
+                if (editAllowed && nameOk && descriptionOk) {
+                    restService.updateToDo(todo).then(function () {
+                        restService.findAllTodos().then(function (data) {
+                            self.todos = data;
+                            self.toggleEditMode(index);
+                            self.currentEdited = null;
+                        });
+                    });
+                }
+            }
+
+            self.changeEditMode = function (index) {
+                if (!self.alreadyEditing) {
+                    self.toggleEditMode(index);
+                    self.currentEdited = index;
+                }
+            }
+
+            self.toggleEditMode = function (index) {
+                self.editMode[index] = self.editMode[index] ? false : true;
+                self.alreadyEditing = self.alreadyEditing ? false : true;
             }
         });
 })();
